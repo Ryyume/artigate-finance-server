f3bede99988117706d42917e84ac042156466203
Ryyume <artigatekz@gmail.com>
2025-05-16 23:44:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import func
+from typing import List
+
 import models, schemas, crud
 from database import SessionLocal, engine
 
@@ -22,6 +24,8 @@ def get_db():
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# === POST ===
+
 @app.post("/subdivisions/", response_model=schemas.Subdivision)
 def add_subdivision(sub: schemas.SubdivisionCreate, db: Session = Depends(get_db)):
     return crud.create_subdivision(db, sub)
@@ -37,6 +41,16 @@ def add_sale(sale: schemas.SaleCreate, db: Session = Depends(get_db)):
 @app.post("/expenses/", response_model=schemas.Expense)
 def add_expense(exp: schemas.ExpenseCreate, db: Session = Depends(get_db)):
     return crud.create_expense(db, exp)
+
+# === GET ===
+
+@app.get("/subdivisions/", response_model=List[schemas.Subdivision])
+def get_subdivisions(db: Session = Depends(get_db)):
+    return db.query(models.Subdivision).all()
+
+@app.get("/sales/", response_model=List[schemas.Sale])
+def get_sales(db: Session = Depends(get_db)):
+    return db.query(models.Sale).all()
 
 @app.get("/stats", response_class=HTMLResponse)
 def stats(request: Request, db: Session = Depends(get_db)):
